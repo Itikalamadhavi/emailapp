e0d74d3c69f1993049085a3913d55a1c2c0668a8
Itikalamadhavi <itikalamadhavi007@gmail.com>
2021-11-21 12:58:23 +0530

    created!

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AccountComponent } from './account/account.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    SignupComponent,
+    AccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
